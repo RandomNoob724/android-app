5724a047831ff982206335d220542cd41caadddc
Emil Persson <emilper1999@gmail.com>
2020-02-02 15:04:54 +0100

    initiated activities

---
+++ app/src/main/java/com/example/workout_diary/ConfigureWorkoutActivity.kt
@@ -0,0 +1,12 @@
+package com.example.workout_diary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfigureWorkoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_configure_workout)
+    }
+}
---
+++ app/src/main/java/com/example/workout_diary/YourWeekActivity.kt
@@ -0,0 +1,12 @@
+package com.example.workout_diary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class YourWeekActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_your_week)
+    }
+}
