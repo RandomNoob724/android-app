41559f18b4925289936611607520a2963bffb983
Emil <emilper1999@gmail.com>
2020-02-26 17:19:40 +0100

    Added hamburger menu

--- app/src/main/java/com/example/workout_diary/MainActivity.kt
+++ app/src/main/java/com/example/workout_diary/MainActivity.kt
@@ -5,12 +5,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.*
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var drawer: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout)
+
+        val toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
 
         val mainCalendarView = this.findViewById<CalendarView>(R.id.main_calendar_view)
         val todaysActivities = this.findViewById<ListView>(R.id.main_list_view)
@@ -27,6 +43,14 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, ViewExercise::class.java)
             intent.putExtra(ViewExercise.EXERCISE_ID, exerciseItem.id)
             startActivity(intent)
+        }
+    }
+
+    override fun onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.END)
+        } else {
+            super.onBackPressed()
         }
     }
 }
